00cfc38821c66e23d63313b7cdcf35cb3896dc54
prasadkjose <prasadkjose@gmail.com>
2017-12-23 12:01:35 +0530

    postgres conn

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const hbs = require('hbs');
- var mysql = require('mysql');
+const { Client } = require('pg');
 var path = require('path');
 
 const port = process.env.PORT || 3000;
 var app = express();
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "faultress"
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
-
+client.connect();
 
 
 
@@ -23,14 +21,14 @@ app.use('/ng-gentelella', express.static(path.join(__dirname, 'node_modules', 'n
 
 app.get('/', (req, res) => {
   var dataList= [];
-  con.query("SELECT * FROM test", function (err, rows, fields) {
+  client.query("SELECT * FROM test", function (err, rows, fields) {
     if (err) throw err;
     for (var i = 0 ; i < rows.length; i++)
                    {
 
                       // Create an object to save current row's data
                       var data = {
-                          'timestamp':rows[i].timestamp,
+
                           'Line':rows[i].line,
                           'Section':rows[i].section,
                           'category':rows[i].category
