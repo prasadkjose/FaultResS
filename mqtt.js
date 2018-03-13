c0ea3cf326c3f14afefb2448845fe840b0c58505
prasadkjose <prasadkjose@gmail.com>
2018-03-13 17:32:48 +0530

    mqtt client using server sent events

--- mqtt.js
+++ mqtt.js
@@ -1,5 +1,5 @@
 var mqtt = require('mqtt');
-var MQTT_ADDR           = "mqtt://localhost:1883";
+var MQTT_ADDR           = "mqtt://52.172.25.136:1883";
 var MQTT_PORT           = 1883;
 var client  = mqtt.connect(MQTT_ADDR,{protocolId: 'MQIsdp', protocolVersion: 3, connectTimeout:1000, debug:true});
 
